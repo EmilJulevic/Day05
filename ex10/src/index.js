555bfe1fe9774c63fda7c0d0cd5ef4d61e07c3c0
EmilJulevic <emiljulevic@live.com>
2021-03-12 23:15:20 +0100

    Adding ex10 folder

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,10 +1,10 @@
 function main() {
-    
+
     var myString = "Jello World";
     myString = "Hello World";
-    
-    
-        return myString;
-    }
-    console.log(main());
-    module.exports = main;
+
+
+    return myString;
+}
+console.log(main());
+module.exports = main;
