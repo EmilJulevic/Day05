c3c800329c3c6e42ecd2b3bc41e8581e1af1f2b9
EmilJulevic <emiljulevic@live.com>
2021-03-12 23:12:48 +0100

    Adding ex06 folder

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,10 +1,10 @@
 function main() {
     var myName = "Admir"
     var myString = "My name is " + myName + " and I am well!";
-    
-    
-    
-        return myString;
-    }
-    console.log(main());
-    module.exports = main;
+
+
+
+    return myString;
+}
+console.log(main());
+module.exports = main;
