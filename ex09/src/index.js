a7f84facfbbe754ceb6c74807190879025c70e8c
EmilJulevic <emiljulevic@live.com>
2021-03-12 23:14:47 +0100

    Adding ex09 folder

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,10 +1,10 @@
 function main() {
-    
+
     var lastName = "Doe";
     var firstLetterOfLastName = lastName[0];
-    
-    
-        return firstLetterOfLastName;
-    }
-    console.log(main());
-    module.exports = main;
+
+
+    return firstLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
